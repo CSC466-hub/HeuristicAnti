bc03712ff453528cc25eacb9875882ab9a58b39c
CSC466-hub <70179088+CSC466-hub@users.noreply.github.com>
2021-08-17 18:38:11 -0400

    Add files via upload

---
+++ Heuristic.py
@@ -0,0 +1,56 @@
+import pefile
+import sys
+import os
+
+for entry in os.scandir(os.getcwd()):
+
+	if(entry.path.endswith(".py")):
+	
+		continue
+		
+	else:
+	
+		array = []
+		malware_file = entry.path
+		pe = pefile.PE(malware_file)
+		print(entry.path)
+		
+		if hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
+			for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+				array.append(hex(exp.address + pe.OPTIONAL_HEADER.IMAGEBASE))
+				
+		array.sort()
+		
+		for i in array:
+			if array.count(i) > 2:
+				print("")
+				
+				break
+				
+		arrayHelper = []
+		
+		for i in range(1, len(array)):
+			arrayHelper.append((int(array[i],16) - int(array[i-1], 16)))
+			
+		for i in range(len*arrayHelper)-1):
+			j = 0
+			
+			while(arrayHelper[i+j]==0):
+				if((i + j) > (len(arrayHelper)-2)):
+					break
+					
+				j = j + 1
+				
+			while(j != 0):
+				j = j - 1
+				
+				arrayHelper[] = arrayHelper[i+j+1]
+				
+		
+		for i in arrayHelper:
+		
+			if arrayHelper.count(i) > 2:
+			
+				print("")
+				
+				break
